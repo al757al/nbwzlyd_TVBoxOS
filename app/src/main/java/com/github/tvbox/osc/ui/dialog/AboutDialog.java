c6767404aeb37db8bfa6ca615a0309f02a189e36
李耀东 <liyaodong@soulapp.cn>
2022-09-23 11:25:09 +0800

    v.1.0.5 文案修改

--- app/src/main/java/com/github/tvbox/osc/ui/dialog/AboutDialog.java
+++ app/src/main/java/com/github/tvbox/osc/ui/dialog/AboutDialog.java
@@ -22,7 +22,7 @@ public class AboutDialog extends BaseDialog {
         TextView textView = findViewById(R.id.title1);
 
         SpanUtils.with(textView).append("版本号 V"+ BuildConfig.VERSION_NAME+"更新日志:")
-                .append("\n").append("1.调整首页历史等按钮的大小，适配TV更大气\n2.仓库推送顺序改为正向排序\n3.首页长按遥控器菜单键跳转设置页，短按切花首页源" +
+                .append("\n").append("1.调整首页历史等按钮的大小，适配TV更大气\n2.仓库推送顺序改为正向排序\n3.首页长按遥控器菜单键跳转设置页，短按切换首页源" +
                         "\n4.修复覆盖安装点击设置页会崩溃的问题\n6.删除内置赞赏码和公众号，起了一个非常不好的头，抱歉\n7.支持反编译内置仓库，beta版本，自行研究不一定生效" +
                         "\n8.支持首页换线路了，不用再跳转设置页了，撒花~").create();
     }
