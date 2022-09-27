d98ac7e8881f38c39c6969dd490ec76cae092f5d
李耀东 <liyaodong@soulapp.cn>
2022-09-27 16:03:00 +0800

    v.1.0.7 转kotlin

--- app/src/main/java/com/github/tvbox/osc/ui/dialog/AboutDialog.kt
+++ app/src/main/java/com/github/tvbox/osc/ui/dialog/AboutDialog.kt
@@ -1,28 +1,29 @@
-package com.github.tvbox.osc.ui.dialog;
+package com.github.tvbox.osc.ui.dialog
 
-import android.content.Context;
-import android.widget.TextView;
+import android.content.Context
+import android.widget.TextView
+import com.blankj.utilcode.util.SpanUtils
+import com.github.tvbox.osc.BuildConfig
+import com.github.tvbox.osc.R
 
-import androidx.annotation.NonNull;
-
-import com.blankj.utilcode.util.SpanUtils;
-import com.github.tvbox.osc.BuildConfig;
-import com.github.tvbox.osc.R;
-
-import org.jetbrains.annotations.NotNull;
-
-public class AboutDialog extends BaseDialog {
-
-    public AboutDialog(@NonNull @NotNull Context context) {
-        super(context);
-        setContentView(R.layout.dialog_about);
-        TextView textView = findViewById(R.id.title1);
-
-        SpanUtils.with(textView).append("版本号 V"+ BuildConfig.VERSION_NAME+"更新日志:")
-                .append("\n").append("1.设置页UI优化，感谢双喜大兄弟\n2.增加清除缓存功能，让你的app重获新生\n3.优化遥控器焦点，点开就能看到我上次选的哪个条目" +
-                        "\n4.进度条有颜色了，视频进度一看便知\n5.修复内存泄漏，占用内存少了\n5.优化了线路刷新逻辑，不会闪动了" +
-                        "\n7.支持远程推送多仓库链接了~ \n8.手机支持拖拽对仓库进行排序了(电视不支持)~" +
-                        "\n9.首页loading时点击返回可以强制关闭loading了，避免阻塞后续操作。心急的大佬不用一直等待~" +
-                        "\n10.修复推送剪切板崩溃~").create();
+class AboutDialog(context: Context) : BaseDialog(context) {
+    init {
+        setContentView(R.layout.dialog_about)
+        val textView = findViewById<TextView>(R.id.title1)
+        SpanUtils.with(textView).append("版本号 V" + BuildConfig.VERSION_NAME + "更新日志:")
+            .append("\n").append(
+                """
+    1.设置页UI优化，感谢双喜大兄弟
+    2.增加清除缓存功能，让你的app重获新生
+    3.优化遥控器焦点，点开就能看到我上次选的哪个条目
+    4.进度条有颜色了，视频进度一看便知
+    5.修复内存泄漏，占用内存少了
+    5.优化了线路刷新逻辑，不会闪动了
+    7.支持远程推送多仓库链接了~ 
+    8.手机支持拖拽对仓库进行排序了(电视不支持)~
+    9.首页loading时点击返回可以强制关闭loading了，避免阻塞后续操作。心急的大佬不用一直等待~
+    10.修复推送剪切板崩溃~
+    """.trimIndent()
+            ).create()
     }
 }
