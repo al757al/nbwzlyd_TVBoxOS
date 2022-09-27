e56d5acd4821dbd498ea37efd4aa5698cdcd95c6
李耀东 <liyaodong@soulapp.cn>
2022-09-27 10:48:26 +0800

    v.1.0.7 增加版本更新日志

--- app/src/main/java/com/github/tvbox/osc/ui/dialog/AboutDialog.java
+++ app/src/main/java/com/github/tvbox/osc/ui/dialog/AboutDialog.java
@@ -21,6 +21,7 @@ public class AboutDialog extends BaseDialog {
         SpanUtils.with(textView).append("版本号 V"+ BuildConfig.VERSION_NAME+"更新日志:")
                 .append("\n").append("1.设置页UI优化，感谢双喜大兄弟\n2.增加清除缓存功能，让你的app重获新生\n3.优化遥控器焦点，点开就能看到我上次选的哪个条目" +
                         "\n4.进度条有颜色了，视频进度一看便知\n5.修复内存泄漏，占用内存少了\n5.优化了线路刷新逻辑，不会闪动了" +
-                        "\n7.支持远程推送多仓库链接了~ \n8.手机支持拖拽对仓库进行排序了(电视不支持)~").create();
+                        "\n7.支持远程推送多仓库链接了~ \n8.手机支持拖拽对仓库进行排序了(电视不支持)~" +
+                        "\n9.首页loading时点击返回可以强制关闭loading了，避免阻塞后续操作。心急的大佬不用一直等待~").create();
     }
 }
