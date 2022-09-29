82acdc2f5d13c04746bf341e06f10245ca5e6373
李耀东 <liyaodong@soulapp.cn>
2022-09-29 15:33:19 +0800

    v.1.0.8 升级版本

--- app/src/main/java/com/github/tvbox/osc/ui/dialog/AboutDialog.kt
+++ app/src/main/java/com/github/tvbox/osc/ui/dialog/AboutDialog.kt
@@ -12,17 +12,11 @@ class AboutDialog(context: Context) : BaseDialog(context) {
         val textView = findViewById<TextView>(R.id.title1)
         SpanUtils.with(textView).append("版本号 V" + BuildConfig.VERSION_NAME + "更新日志:")
             .append("\n").append(
-                """
-    1.设置页UI优化，感谢双喜大兄弟
-    2.增加清除缓存功能，让你的app重获新生
-    3.优化遥控器焦点，点开就能看到我上次选的哪个条目
-    4.进度条有颜色了，视频进度一看便知
-    5.修复内存泄漏，占用内存少了
-    5.优化了线路刷新逻辑，不会闪动了
-    7.支持远程推送多仓库链接了~ 
-    8.手机支持拖拽对仓库进行排序了(电视不支持)~
-    9.首页loading时点击返回可以强制关闭loading了，避免阻塞后续操作。心急的大佬不用一直等待~
-    10.修复推送剪切板崩溃~
+                """    
+                    1.修复了点击返回强制关闭loading不生效问题
+                    2.修复由于强制关闭loading导致资源下载不完全，每次会加载jar失败的问题
+                    3.修复无法退出app的问题
+                    强烈建议更新~
     """.trimIndent()
             ).create()
     }
