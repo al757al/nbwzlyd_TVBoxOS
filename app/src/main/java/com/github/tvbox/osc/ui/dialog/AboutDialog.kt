785332fec31482e60d577e19541e90a9b2b5e747
derek <liyaodong@soulapp.cn>
2022-10-05 16:25:28 +0800

    v.1.0.9 升级版本

--- app/src/main/java/com/github/tvbox/osc/ui/dialog/AboutDialog.kt
+++ app/src/main/java/com/github/tvbox/osc/ui/dialog/AboutDialog.kt
@@ -13,10 +13,7 @@ class AboutDialog(context: Context) : BaseDialog(context) {
         SpanUtils.with(textView).append("版本号 V" + BuildConfig.VERSION_NAME + "更新日志:")
             .append("\n").append(
                 """    
-                    1.修复了点击返回强制关闭loading不生效问题
-                    2.修复由于强制关闭loading导致资源下载不完全，每次会加载jar失败的问题
-                    3.修复无法退出app的问题
-                    强烈建议更新~
+                   更新日志较多，参考公众号  （安卓哥开发）
     """.trimIndent()
             ).create()
     }
