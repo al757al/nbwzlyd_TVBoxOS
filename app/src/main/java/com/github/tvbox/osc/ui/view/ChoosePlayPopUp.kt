4ce4fcb2cc57cbd52d535b501b8f52655a0366e0
derek <liyaodong@soulapp.cn>
2022-10-07 17:33:57 +0800

    v.1.0.11 ui调整

--- app/src/main/java/com/github/tvbox/osc/ui/view/ChoosePlayPopUp.kt
+++ app/src/main/java/com/github/tvbox/osc/ui/view/ChoosePlayPopUp.kt
@@ -2,11 +2,15 @@ package com.github.tvbox.osc.ui.view
 
 import android.content.Context
 import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Rect
 import android.view.View
+import com.blankj.utilcode.util.ScreenUtils
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.github.tvbox.osc.R
 import com.github.tvbox.osc.base.App
 import com.github.tvbox.osc.bean.VodInfo
+import com.github.tvbox.osc.bean.VodInfo.VodSeries
 import com.github.tvbox.osc.cache.RoomDataManger
 import com.github.tvbox.osc.event.RefreshEvent
 import com.github.tvbox.osc.ui.activity.PlayEvent
@@ -30,39 +34,62 @@ class ChoosePlayPopUp(context: Context?) : BasePopupWindow(context) {
             setHasFixedSize(true)
             layoutManager = V7GridLayoutManager(getContext(), 6)
             adapter = seriesAdapter
+            isFocusable = true
+            isFocusableInTouchMode = true
+            requestFocus()
         }
-        seriesAdapter?.onItemClickListener = object : BaseQuickAdapter.OnItemClickListener {
-            override fun onItemClick(adapter: BaseQuickAdapter<*, *>?, view: View?, position: Int) {
+        val vodInfo = App.instance?.getVodInfo()
+
+        val list: MutableList<VodSeries> =
+            vodInfo?.seriesMap?.get(vodInfo.playFlag) ?: mutableListOf()
+
+
+        seriesAdapter?.onItemClickListener =
+            BaseQuickAdapter.OnItemClickListener { adapter, view, position ->
                 FastClickCheckUtil.check(view)
-                val vodInfo = App.instance?.getVodInfo() ?: return
-                if ((vodInfo.seriesMap.get(vodInfo.playFlag)?.size ?: 0) > 0) {
-                    for (j in vodInfo.seriesMap.get(vodInfo.playFlag)?.indices!!) {
+                if ((list.size) > 0) {
+                    for (j in list.indices) {
                         seriesAdapter!!.data[j].selected = false
                         seriesAdapter!!.notifyItemChanged(j)
                     }
                     //解决倒叙不刷新
-                    if (vodInfo.playIndex != position) {
+                    if (vodInfo?.playIndex != position) {
                         seriesAdapter!!.data[position].selected = true
                         seriesAdapter!!.notifyItemChanged(position)
-                        vodInfo.playIndex = position
+                        vodInfo?.playIndex = position
                     }
-                    seriesAdapter!!.data[vodInfo.playIndex].selected = true
-                    seriesAdapter!!.notifyItemChanged(vodInfo.playIndex)
-                    insertVod(vodInfo)
+                    seriesAdapter!!.data[vodInfo?.playIndex ?: 0].selected = true
+                    seriesAdapter!!.notifyItemChanged(vodInfo?.playIndex ?: 0)
+                    insertVod(vodInfo ?: return@OnItemClickListener)
                     EventBus.getDefault().post(PlayEvent().apply {
                         this.vodInfo = vodInfo
                     })
                 }
             }
-
-        }
         setBackgroundColor(Color.TRANSPARENT)
+        val pFont = Paint()
+        val rect = Rect()
+        var w = 1
+        for (i in list.indices) {
+            val name = list[i].name
+            pFont.getTextBounds(name, 0, name.length, rect)
+            if (w < rect.width()) {
+                w = rect.width()
+            }
+        }
+        w += 32
+        val screenWidth: Int = ScreenUtils.getScreenWidth() / 3
+        var offset = screenWidth / w
+        if (offset <= 1) offset = 1
+        if (offset > 6) offset = 6
+        (this.mGridView?.layoutManager as V7GridLayoutManager).spanCount = offset
     }
 
     override fun showPopupWindow(anchorView: View?) {
         super.showPopupWindow(anchorView)
         popupWindow.isFocusable = true
-        seriesAdapter?.setNewData(App.instance?.getVodInfo()?.seriesMap?.get(App.instance?.getVodInfo()?.playFlag))
+        val vodInfo = App.instance?.getVodInfo()
+        seriesAdapter?.setNewData(vodInfo?.seriesMap?.get(vodInfo.playFlag))
     }
 
     private fun insertVod(vodInfo: VodInfo) {
