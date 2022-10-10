c0761d13ded410067d4ce1f59fc19953807981c4
nbwzlyd <420907013@qq.com>
2022-10-10 19:56:20 +0800

    v.1.0.12 增加是否是电视端的方法校验

--- app/src/main/java/com/github/tvbox/osc/util/ScreenUtils.java
+++ app/src/main/java/com/github/tvbox/osc/util/ScreenUtils.java
@@ -33,7 +33,7 @@ public class ScreenUtils {
 
     public static boolean isTv(Context context) {
         UiModeManager uiModeManager = (UiModeManager) context.getSystemService(UI_MODE_SERVICE);
-        return uiModeManager.getCurrentModeType() == Configuration.UI_MODE_TYPE_TELEVISION || checkScreenLayoutIsTv(context) && !checkIsPhone(context);
+        return uiModeManager.getCurrentModeType() == Configuration.UI_MODE_TYPE_TELEVISION || (checkScreenLayoutIsTv(context) && !checkIsPhone(context));
     }
 
 
