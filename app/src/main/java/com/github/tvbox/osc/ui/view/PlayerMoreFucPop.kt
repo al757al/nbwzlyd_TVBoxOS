8a2afc9bd6a7efa7acbc905dfcc0d3db006e0382
nbwzlyd <420907013@qq.com>
2022-10-21 17:21:12 +0800

    v.1.0.15 播放页popup

---
+++ app/src/main/java/com/github/tvbox/osc/ui/view/PlayerMoreFucPop.kt
@@ -0,0 +1,21 @@
+package com.github.tvbox.osc.ui.view
+
+import android.content.Context
+import android.view.View
+import com.github.tvbox.osc.R
+import razerdp.basepopup.BasePopupWindow
+
+/**
+ * <pre>
+ *     author : derek
+ *     time   : 2022/10/21
+ *     desc   :
+ *     version:
+ * </pre>
+ */
+class PlayerMoreFucPop(context: Context?) : BasePopupWindow(context) {
+
+    init {
+        setContentView(R.layout.player_more_fuc_pop)
+    }
+}
