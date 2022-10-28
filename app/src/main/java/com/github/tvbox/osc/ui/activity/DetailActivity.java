4d7041ebc5c71f146a796d035b9cd14ffac96b77
nbwzlyd <nbwzlyd>
2022-10-28 19:32:41 +0800

    v.1.0.16 设置默认亮度修复片尾bug

--- app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
@@ -14,6 +14,7 @@ import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -896,6 +897,11 @@ public class DetailActivity extends BaseActivity {
 
         if (playFragment.mController != null) {
             playFragment.mController.setIsFullScreen(fullWindows);
+        }
+        if (fullWindows) {
+            playFragment.initDefaultBright(this, 0.43f);
+        } else {
+            playFragment.initDefaultBright(this, WindowManager.LayoutParams.BRIGHTNESS_OVERRIDE_NONE);
         }
     }
 }
--- app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
@@ -14,6 +14,7 @@ import android.os.Handler;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.webkit.ConsoleMessage;
 import android.webkit.JsPromptResult;
 import android.webkit.JsResult;
@@ -124,6 +125,16 @@ public class PlayFragment extends BaseLazyFragment {
         initView();
         initViewModel();
         initData();
+    }
+
+
+    public void initDefaultBright(Context context, float brightLevel) {
+        if (context instanceof Activity) {
+            Activity activity = (Activity) context;
+            WindowManager.LayoutParams lp = activity.getWindow().getAttributes();
+            lp.screenBrightness = brightLevel;
+            activity.getWindow().setAttributes(lp);
+        }
     }
 
     private void initView() {
