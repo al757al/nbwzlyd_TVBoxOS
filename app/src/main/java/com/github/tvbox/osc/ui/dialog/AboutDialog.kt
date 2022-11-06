df6763dca7d6c63a105e4fe109b85aacc80ed965
nbwzlyd <nbwzlyd>
2022-11-06 17:06:23 +0800

    v.1.0.18 日志

--- app/src/main/java/com/github/tvbox/osc/ui/dialog/AboutDialog.kt
+++ app/src/main/java/com/github/tvbox/osc/ui/dialog/AboutDialog.kt
@@ -11,7 +11,7 @@ class AboutDialog(context: Context) : BaseDialog(context) {
     init {
         setContentView(R.layout.dialog_about)
         val textView = findViewById<TextView>(R.id.title1)
-        SpanUtils.with(textView).append("           公众号 安卓哥开发\n\n         ").setBold().setForegroundColor(
+        SpanUtils.with(textView).append("           公众号 安卓哥开发\n\n").setBold().setForegroundColor(
             Color.RED
         ).
         append("版本号 V" + BuildConfig.VERSION_NAME + "更新日志:")
@@ -22,12 +22,10 @@ class AboutDialog(context: Context) : BaseDialog(context) {
 3.播放页不常用按钮聚合
 4.设置页UI改版
 5.直播增加二次确认退出，避免意外退出问题
-6.直播优化
+6.直播优化，避免闪退和无效动画刷新
 7.增加搜索线程
 8.同步q版代码，但不是最新
 10.增加沉浸式开关
-
-
     """.trimIndent()
             ).create()
     }
