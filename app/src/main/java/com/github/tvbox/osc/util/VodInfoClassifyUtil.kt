8d73b0937885b4ca5bb5c24784dfb4678549d712
nbwzlyd <nbwzlyd>
2022-11-10 20:19:22 +0800

    v.1.0.20 播放页集数分组逻辑 优化重置app没有删除历史记录问题

---
+++ app/src/main/java/com/github/tvbox/osc/util/VodInfoClassifyUtil.kt
@@ -0,0 +1,63 @@
+package com.github.tvbox.osc.util
+
+import com.github.tvbox.osc.bean.VodInfo
+
+/**
+ * <pre>
+ *     author : derek
+ *     time   : 2022/11/10
+ *     desc   :
+ *     version:
+ * </pre>
+ */
+class VodInfoClassifyUtil {
+    companion object {
+        @JvmStatic
+        fun checkVodInfoNeedClassify(vodSeries: MutableList<VodInfo.VodSeries>?): Boolean {
+            return (vodSeries?.size ?: 0) > 20
+        }
+
+        @JvmStatic
+        fun getClassifyData(vodSeries: List<VodInfo.VodSeries>?): LinkedHashMap<VodInfo.VodSeriesFlag, List<VodInfo.VodSeries>> {
+            return averageAssignFixLength(vodSeries, 20)
+        }
+
+        private fun <T> averageAssignFixLength(
+            source: List<T>?,
+            splitItemNum: Int
+        ): LinkedHashMap<VodInfo.VodSeriesFlag, List<T>> {
+            val result = LinkedHashMap<VodInfo.VodSeriesFlag, List<T>>()
+            if (source != null && source.run { isNotEmpty() } && splitItemNum > 0) {
+                if (source.size <= splitItemNum) {
+                    return result
+                } else {
+                    // 计算拆分后list数量
+                    val splitNum =
+                        if (source.size % splitItemNum == 0) source.size / splitItemNum else source.size / splitItemNum + 1
+
+                    var value: List<T>?
+                    for (i in 0 until splitNum) {
+                        value = if (i < splitNum - 1) {
+                            source.subList(i * splitItemNum, (i + 1) * splitItemNum)
+                        } else {
+                            // 最后一组
+                            source.subList(i * splitItemNum, source.size)
+                        }
+                        val vodSeriesFlag = VodInfo.VodSeriesFlag().apply {
+                            var endInt = (i + 1) * 20
+                            if (endInt > source.size) {
+                                endInt = source.size
+                            }
+                            this.name = "${i * 20 + 1}-${endInt}"
+                            this.selected = i == 0
+                        }
+                        result[vodSeriesFlag] = value
+                    }
+                }
+            }
+
+            return result
+        }
+
+    }
+}
