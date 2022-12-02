b492cd38729e20b7c688d246bc3af196b217216c
nbwzlyd <nbwzlyd>
2022-12-02 23:19:29 +0800

    V2.0.6 日志

--- app/src/main/java/com/github/tvbox/osc/ui/dialog/AboutDialog.kt
+++ app/src/main/java/com/github/tvbox/osc/ui/dialog/AboutDialog.kt
@@ -15,16 +15,8 @@ class AboutDialog(context: Context) : BaseDialog(context) {
             .append(BuildConfig.VERSION_NAME + "更新日志:")
             .append("\n").append(
                 """  
-                    1、首页数据源增加动态列数
-                    2、首页数据源推送适配刚刚接口
-                    3、焦点问题优化
-                    4、网盘内置字幕格式兼容，解决部分字幕无法加载问题
-                    5、网盘缓存时间改为2min
-                    6、支持alistV3网盘
-                    7、支持直播地址复制
-                    8、jar弹框文案提示优化
-                    9、修复直播有密码时无法进行线路切换操作问题
-                    10、支持锁屏
+                    1、支持mitv P2p直播格式
+                    2、支持m3u直播格式
 
     """.trimIndent()
             ).create()
