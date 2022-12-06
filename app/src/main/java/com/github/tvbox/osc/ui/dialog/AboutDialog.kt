88eedf3c66a8cc0fe04f88124cc9b5e8250a749a
nbwzlyd <nbwzlyd>
2022-12-06 21:55:38 +0800

    V3.0 ui

--- app/src/main/java/com/github/tvbox/osc/ui/dialog/AboutDialog.kt
+++ app/src/main/java/com/github/tvbox/osc/ui/dialog/AboutDialog.kt
@@ -18,6 +18,9 @@ class AboutDialog(context: Context) : BaseDialog(context) {
                     1、支持mitv P2p直播格式
                     2、支持m3u直播格式
                     3、视频支持悬浮窗
+                    4、增加主页长按可以刷新接口和缓存逻辑
+                    5、直播缓存10L,提升加载速度
+                    6、修复切换播放器会导致焦点丢失的问题
 
     """.trimIndent()
             ).create()
