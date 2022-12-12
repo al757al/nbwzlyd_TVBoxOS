6bfe215c9ea75e778eaf0ef70ef1e6b2c4277100
nbwzlyd <nbwzlyd>
2022-12-12 12:06:56 +0800

    V3.0 回退exo播放器

--- app/src/main/java/com/github/tvbox/osc/ui/dialog/AboutDialog.kt
+++ app/src/main/java/com/github/tvbox/osc/ui/dialog/AboutDialog.kt
@@ -22,7 +22,6 @@ class AboutDialog(context: Context) : BaseDialog(context) {
                     5、直播缓存10H,提升加载速度
                     6、修复切换播放器会导致焦点丢失的问题
                     7、尝试解决surfaceView渲染切后台再切前台导致的黑屏问题
-                    8、升级exo播放器到2.18.1
 
     """.trimIndent()
             ).create()
