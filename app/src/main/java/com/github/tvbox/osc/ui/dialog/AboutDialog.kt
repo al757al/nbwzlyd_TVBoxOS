b4dceb594d7dec48f8ca7afea65a33800b080a65
nbwzlyd <nbwzlyd>
2023-01-01 11:22:20 +0800

    V4.0.2 日志

--- app/src/main/java/com/github/tvbox/osc/ui/dialog/AboutDialog.kt
+++ app/src/main/java/com/github/tvbox/osc/ui/dialog/AboutDialog.kt
@@ -18,7 +18,8 @@ class AboutDialog(context: Context) : BaseDialog(context) {
                     1、直播/影视增加边缘切换模式，可在边缘滑动切换频道/上下集了
                     2、影视，直播播放器解耦，直播长按播放器可单独设置直播播放器
                     3、选集模式优化，避免过早消失
-                    4、其他小优化
+                    4、优化直播线路偶现的错乱问题
+                    5、其他UI小优化
     """.trimIndent()
             ).create()
     }
